271e70ad1361073ee87ec6a716a52abd87be9642
InstincT <113701884+monish-instinct@users.noreply.github.com>
2024-06-05 14:40:06 +0530

    Add files via upload

---
+++ Sources/sample package/File.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by InstincT on 05/06/24.
+//
+
+import Foundation
+
+public class instinct {
+    public static func sayHello() {
+        print("Hello from InstincT!")
+    }
+}
