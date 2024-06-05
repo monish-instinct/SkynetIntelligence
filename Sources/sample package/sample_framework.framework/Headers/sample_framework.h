7d3932c6cdaf0bbee5b7ac859742a4734c813df0
InstincT <113701884+monish-instinct@users.noreply.github.com>
2024-06-05 15:14:51 +0530

    Add files via upload

---
+++ Sources/sample package/sample_framework.framework/Headers/sample_framework.h
@@ -0,0 +1,18 @@
+//
+//  sample_framework.h
+//  sample framework
+//
+//  Created by InstincT on 05/06/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for sample_framework.
+FOUNDATION_EXPORT double sample_frameworkVersionNumber;
+
+//! Project version string for sample_framework.
+FOUNDATION_EXPORT const unsigned char sample_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sample_framework/PublicHeader.h>
+
+
