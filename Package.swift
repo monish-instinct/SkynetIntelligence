c671f66be00a1daba28a6bfac7b6a48d6060b502
InstincT <113701884+monish-instinct@users.noreply.github.com>
2024-06-06 10:05:45 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,13 +1,10 @@
-// swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 // swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
     name: "SamplePackage",
     platforms: [
-        .iOS(.v9),
+        .iOS(.v12), // Updated to minimum supported version
         .macOS(.v10_15),
     ],
     products: [
@@ -18,9 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SampleFramework",
-            url: "https://github.com/monish-instinct/swift-package/releases/download/swift/SampleFramework.xcframework.zip",
-            checksum: "9050ae8b5fdb181ca902ad0cb46e317e002e5afed6f594550a972c2dff466810"
+            url: "https://github.com/monish-instinct/swift-package/releases/download/v0.0.1/SampleFramework.xcframework.zip",
+            checksum: "9050ae8b5fdb181ca902ad0cb46e317e002e5afed6f594550a972c2dff466810" // Ensure this is correct
         )
     ]
 )
-
