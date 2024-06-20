1cbe4c6ff8f8cca1a460291572de2fac0e6faa6e
InstincT <113701884+monish-instinct@users.noreply.github.com>
2024-06-20 11:14:49 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,13 +10,13 @@ let package = Package(
     products: [
         .library(
             name: "SamplePackage",
-            targets: ["SampleFramework"]),
+            targets: ["TestFramework"]),
     ],
     targets: [
         .binaryTarget(
-            name: "SampleFramework",
-            url: "https://github.com/monish-instinct/swift-package/releases/download/swift/SampleFramework.xcframework.zip",
-            checksum: "9050ae8b5fdb181ca902ad0cb46e317e002e5afed6f594550a972c2dff466810" // Ensure this is correct
+            name: "TestFramework",
+            url: "https://github.com/monish-instinct/swift-package/releases/download/swift/TestFramework.xcframework.zip",
+            checksum: "e18ce794f12e50c4a7efeb4cc62cae7828076de03aacb346d3fe5dc763a7988d" // Ensure this is correct
         )
     ]
 )
